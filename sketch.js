f7324947b9425b2a2b0bb634ee4c492ed9c17efd
Shounok-8 <79404812+Shounok-8@users.noreply.github.com>
2021-05-19 14:44:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var heart
+
+function preload(){
+    heart = loadImage("image/download.png");
+}
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+}
+
+function draw(){
+    stroke("red");
+  strokeWeight(2);
+  textSize(20);
+  text("Thank you mam for teaching and introducing me about computers and javascript.You will always be my inspiration.I will miss you mam :-)",200, 350)
+  
+    rect(200,200,600,200)
+}
